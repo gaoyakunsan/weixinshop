59e73e8444e4fcdde07608049eebd1abdeb7f472
yakungao <yakungao@ITA-1401-0033.synacast.local>
2017-02-23 19:30:30 +0800

    大

--- weixinshop/src/main/webapp/sql/weixinshopl.sql
+++ weixinshop/src/main/webapp/sql/weixinshopl.sql
@@ -38,6 +38,22 @@ CREATE TABLE `admin` (
 
 insert  into `admin`(`admin_id`,`username`,`password`,`realname`,`email`,`phone`,`isdelete`,`last_modified_time`,`create_time`,`admin_pid`) values (49,'admin','21232F297A57A5A743894A0E4A801FC3','chenzhangsheng12','zzugyk@163.com','18758825854',0,'2017-01-11 11:37:40','2017-01-02 22:10:13',NULL);
 
+/*Table structure for table `map_admin_permission` */
+
+DROP TABLE IF EXISTS `map_admin_permission`;
+
+CREATE TABLE `map_admin_permission` (
+  `map_admin_perm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  `isdelete` tinyint(1) NOT NULL DEFAULT '0',
+  `last_modified_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`map_admin_perm_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `map_admin_permission` */
+
 /*Table structure for table `map_admin_role` */
 
 DROP TABLE IF EXISTS `map_admin_role`;
@@ -50,9 +66,11 @@ CREATE TABLE `map_admin_role` (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_modified_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`map_admin_role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=119 DEFAULT CHARSET=utf8;
 
 /*Data for the table `map_admin_role` */
+
+insert  into `map_admin_role`(`map_admin_role_id`,`admin_id`,`role_id`,`isdelete`,`create_time`,`last_modified_time`) values (117,49,6,0,'2017-02-20 16:13:50','2017-02-20 16:13:50'),(118,49,4,0,'2017-02-20 16:13:50','2017-02-20 16:13:50');
 
 /*Table structure for table `map_role_privilege` */
 
@@ -69,6 +87,25 @@ CREATE TABLE `map_role_privilege` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `map_role_privilege` */
+
+/*Table structure for table `permission` */
+
+DROP TABLE IF EXISTS `permission`;
+
+CREATE TABLE `permission` (
+  `permission_id` int(11) NOT NULL AUTO_INCREMENT,
+  `perm_name` varchar(50) NOT NULL,
+  `perm_des` varchar(100) DEFAULT NULL,
+  `url` varchar(50) DEFAULT NULL,
+  `isdelete` tinyint(1) NOT NULL DEFAULT '0',
+  `last_modified_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`permission_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `permission` */
+
+insert  into `permission`(`permission_id`,`perm_name`,`perm_des`,`url`,`isdelete`,`last_modified_time`,`create_time`) values (1,'系统管理','系统管理权限','systemManage.do',0,'2017-02-22 18:26:21','2017-02-21 10:49:41'),(2,'1122','3344','2233',0,'2017-02-22 19:15:50','2017-02-22 18:54:08');
 
 /*Table structure for table `privilege` */
 
@@ -89,7 +126,7 @@ CREATE TABLE `privilege` (
 
 /*Data for the table `privilege` */
 
-insert  into `privilege`(`privilege_id`,`display_name`,`privilege_name`,`privilege_code`,`url`,`parent_id`,`isdelete`,`create_time`,`last_modified_time`) values (1,'11',NULL,NULL,'11',11,0,'2017-02-15 17:55:01','2017-02-15 17:55:01'),(2,'567567567',NULL,'','11',11,1,'2017-02-15 17:56:39','2017-02-15 19:19:14');
+insert  into `privilege`(`privilege_id`,`display_name`,`privilege_name`,`privilege_code`,`url`,`parent_id`,`isdelete`,`create_time`,`last_modified_time`) values (1,'11',NULL,NULL,'11',11,1,'2017-02-15 17:55:01','2017-02-20 16:19:29'),(2,'567567567',NULL,'','11',11,1,'2017-02-15 17:56:39','2017-02-15 19:19:14');
 
 /*Table structure for table `role` */
 
@@ -104,11 +141,11 @@ CREATE TABLE `role` (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_modified_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 /*Data for the table `role` */
 
-insert  into `role`(`role_id`,`display_name`,`role_name`,`role_code`,`isdelete`,`create_time`,`last_modified_time`) values (3,'系统管理员',NULL,'001',0,'2017-02-16 17:04:06','2017-02-16 17:04:06');
+insert  into `role`(`role_id`,`display_name`,`role_name`,`role_code`,`isdelete`,`create_time`,`last_modified_time`) values (3,'系统管理员',NULL,'001',1,'2017-02-16 17:04:06','2017-02-17 16:04:41'),(4,'视频制作者',NULL,'002',0,'2017-02-17 15:39:07','2017-02-17 15:39:07'),(5,'记者',NULL,'003',0,'2017-02-17 15:39:23','2017-02-17 15:39:23'),(6,'审核',NULL,'004',0,'2017-02-17 15:39:40','2017-02-17 15:39:40'),(7,'编辑者',NULL,'006',1,'2017-02-20 15:49:22','2017-02-20 15:50:10');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
